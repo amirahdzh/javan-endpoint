e1ee43671485a259ebd40b73be80dd34e89136da
amirahdzh <amirahdzh@gmail.com>
2025-10-17 03:06:03 +0700

    change host

--- server.js
+++ server.js
@@ -67,6 +67,6 @@ app.delete("/users/:id", (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server listening on port ${PORT}`);
 });
